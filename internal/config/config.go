b777b795521de4a4dfd9d880490ed7c87ea07726
abhinav <abhinavchauhan593@gmail.com>
2026-06-22 12:28:34 +0530

    fix: viper issue

--- internal/config/config.go
+++ internal/config/config.go
@@ -16,18 +16,21 @@ type Config struct {
 
 
 func LoadConfig() (Config, error) {
+	var config Config 
+
 	viper.SetConfigFile(".env")
-	viper.ReadInConfig()
+
 	viper.AutomaticEnv()
 
-	viper.SetDefault("ENVIRONMENT", "development")
-	viper.SetDefault("SERVER_PORT", "8080")
-
-	var cfg Config
-	if err := viper.ReadInConfig(); err != nil {
-		log.Printf("No .env file found, relying on environment variables")
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Println("No .env file found, relying on environment variables")
 	}
 
-	err := viper.Unmarshal(&cfg)
-	return cfg, err
+	err = viper.Unmarshal(&config)
+	if err != nil {
+		return config, err
+	}
+
+	return config, nil
 }
